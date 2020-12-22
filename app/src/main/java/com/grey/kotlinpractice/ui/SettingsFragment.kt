26ec0e144c7293020cf9d60cec17c16adb826d39
Grey <inertgrey@outlook.com>
2020-12-22 05:00:42 +0200

    minor fixes

--- app/src/main/java/com/grey/kotlinpractice/ui/SettingsFragment.kt
+++ app/src/main/java/com/grey/kotlinpractice/ui/SettingsFragment.kt
@@ -6,16 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.grey.kotlinpractice.R
-import kotlinx.android.synthetic.main.fragment_common.*
+import kotlinx.android.synthetic.main.fragment_search.*
 
 class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_common, container, false)
+        inflater.inflate(R.layout.fragment_search, container, false)
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        tvCommon.text = "settings Fragment"
+        //tvCommon.text = "settings Fragment"
         commonLayout.setBackgroundColor(resources.getColor(android.R.color.holo_orange_dark))
     }
 }
---
+++ app/src/main/java/com/grey/kotlinpractice/utils/XMLParser.kt
@@ -0,0 +1,4 @@
+package com.grey.kotlinpractice.utils
+
+class XMLParser {
+}
